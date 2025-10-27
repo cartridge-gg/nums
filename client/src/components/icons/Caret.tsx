807b3a43727df299b926edb75e58d052305febff
bal7hazar <bal7hazar@proton.me>
2025-10-27 18:28:00 +0100

    fix: typescript issues

--- client/src/components/icons/Caret.tsx
+++ client/src/components/icons/Caret.tsx
@@ -1,16 +1,12 @@
-import { Icon, IconProps } from "@chakra-ui/react";
-
-export const CaretIcon = ({ props }: { props?: IconProps }) => {
+export const CaretIcon = () => {
   return (
-    <Icon boxSize="18px" {...props}>
-      <svg
-        viewBox="0 0 24 24"
-        filter="drop-shadow(2px 2px 0px rgba(0, 0, 0, 0.25))"
-      >
-        <g fill="currentColor">
-          <path d="M16 10.4L12 14.4L8 10.4L8 9.59998L16 9.59998V10.4Z" />
-        </g>
-      </svg>
-    </Icon>
+    <svg
+      viewBox="0 0 24 24"
+      filter="drop-shadow(2px 2px 0px rgba(0, 0, 0, 0.25))"
+    >
+      <g fill="currentColor">
+        <path d="M16 10.4L12 14.4L8 10.4L8 9.59998L16 9.59998V10.4Z" />
+      </g>
+    </svg>
   );
 };

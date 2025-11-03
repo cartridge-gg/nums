a1ca40441ddccfbb7504c65d53241cd04f80c7d8
bal7hazar <bal7hazar@proton.me>
2025-11-03 19:41:41 +0100

    fix: build

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,6 +10,10 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    minify: 'esbuild',
+    chunkSizeWarningLimit: 1000,
+  },
   plugins: [
     react(),
     wasm(),

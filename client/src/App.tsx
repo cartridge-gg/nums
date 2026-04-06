a4b1347a231b253effb67fa15ccb9debce8963b7
Larkooo <larko236@outlook.com>
2026-04-06 13:45:47 -0500

    Remove welcome screen and event ticker
    Co-Authored-By: Lattice <lattice@users.noreply.github.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -22,7 +22,6 @@ import { Game, Home } from "./pages";
 import { queryClient } from "./queries";
 import { QuestsProvider } from "./context/quests";
 import { LoadingProvider } from "./context/loading";
-import { WelcomeProvider } from "./context/welcome";
 import { Layout } from "./components/layouts";
 import { AchievementsProvider } from "./context/achievements";
 import { TutorialProvider } from "./context/tutorial";
@@ -65,47 +64,45 @@ function App() {
                     <PracticeProvider>
                       <QuestsProvider>
                         <AchievementsProvider>
-                          <WelcomeProvider>
-                            <LoadingProvider>
-                              <Router
-                                future={{
-                                  v7_startTransition: true,
-                                  v7_relativeSplatPath: true,
-                                }}
-                              >
-                                <NotificationEvents />
-                                <TutorialProvider>
-                                  <SoundProvider>
-                                    <Layout>
-                                      <Routes>
-                                        <Route path="/" element={<Home />} />
-                                        <Route
-                                          path="/game/:id"
-                                          element={<Game />}
-                                        />
-                                        <Route
-                                          path="/game"
-                                          element={<Navigate to="/" replace />}
-                                        />
-                                        <Route
-                                          path="/practice/:id"
-                                          element={<Game />}
-                                        />
-                                        <Route
-                                          path="/tutorial"
-                                          element={<Game />}
-                                        />
-                                        <Route
-                                          path="/practice"
-                                          element={<Navigate to="/" replace />}
-                                        />
-                                      </Routes>
-                                    </Layout>
-                                  </SoundProvider>
-                                </TutorialProvider>
-                              </Router>
-                            </LoadingProvider>
-                          </WelcomeProvider>
+                          <LoadingProvider>
+                            <Router
+                              future={{
+                                v7_startTransition: true,
+                                v7_relativeSplatPath: true,
+                              }}
+                            >
+                              <NotificationEvents />
+                              <TutorialProvider>
+                                <SoundProvider>
+                                  <Layout>
+                                    <Routes>
+                                      <Route path="/" element={<Home />} />
+                                      <Route
+                                        path="/game/:id"
+                                        element={<Game />}
+                                      />
+                                      <Route
+                                        path="/game"
+                                        element={<Navigate to="/" replace />}
+                                      />
+                                      <Route
+                                        path="/practice/:id"
+                                        element={<Game />}
+                                      />
+                                      <Route
+                                        path="/tutorial"
+                                        element={<Game />}
+                                      />
+                                      <Route
+                                        path="/practice"
+                                        element={<Navigate to="/" replace />}
+                                      />
+                                    </Routes>
+                                  </Layout>
+                                </SoundProvider>
+                              </TutorialProvider>
+                            </Router>
+                          </LoadingProvider>
                         </AchievementsProvider>
                       </QuestsProvider>
                     </PracticeProvider>

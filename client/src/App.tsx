85e4776bd7e811261ab784e9677879f82440bd2a
Larkooo <larko236@outlook.com>
2026-04-06 13:15:11 -0500

    Add tutorial game route
    Co-Authored-By: Lattice <lattice@users.noreply.github.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -92,6 +92,10 @@ function App() {
                                           element={<Game />}
                                         />
                                         <Route
+                                          path="/tutorial"
+                                          element={<Game />}
+                                        />
+                                        <Route
                                           path="/practice"
                                           element={<Navigate to="/" replace />}
                                         />
